32d23e654d179e375821a5ed262ce9ff554621d5
Joshua Buehrer <louisproductions179@gmail.com>
2025-02-21 12:18:58 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Container>
       <Head>
-        <title>Dan's Resume</title>
+        <title>Joshua's Resume</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Body>
